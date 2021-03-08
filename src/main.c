519f99ea479b0047eeb07b0ebbcef68f5985e36f
rexim <reximkut@gmail.com>
2021-03-08 23:40:13 +0700

    Refactor scene reloading procedure

--- src/main.c
+++ src/main.c
@@ -51,7 +51,7 @@ V2 vertex_uvs[VERTEX_CAPACITY];
 size_t vertex_size = 0;
 
 #define MANUAL_TIME_STEP 0.05f
-#define HOT_RELOAD_ERROR_COLOR 0.25f, 0.0f, 0.0f, 1.0f
+#define HOT_RELOAD_ERROR_COLOR 0.5f, 0.0f, 0.0f, 1.0f
 #define BACKGROUND_COLOR 0.0f, 0.0f, 0.0f, 0.0f
 
 char *cstr_from_sv(String_View sv)
@@ -170,224 +170,223 @@ void reload_scene(void)
     const char *texture_file_path = NULL;
     const char *mesh_file_path = NULL;
 
+    glClearColor(HOT_RELOAD_ERROR_COLOR);
+    program_failed = true;
+
+    // reload scene.conf begin
+    {
+        String_View scene_conf_content = sv_from_cstr(slurp_file(scene_conf_file_path));
+        if (scene_conf_content.data == NULL) {
+            return;
+        }
+
+        for (size_t line_number = 0; scene_conf_content.count > 0; line_number++) {
+            String_View line = sv_chop_by_delim(&scene_conf_content, '\n');
+            line = sv_trim(sv_chop_by_delim(&line, '#'));
+
+            if (line.count > 0) {
+                String_View key = sv_trim(sv_chop_by_delim(&line, '='));
+                String_View value = sv_trim(line);
+                if (sv_eq(key, SV("frag_shader"))) {
+                    fragment_shader_file_path = cstr_from_sv(value);
+                } else if (sv_eq(key, SV("vert_shader"))) {
+                    vertex_shader_file_path = cstr_from_sv(value);
+                } else if (sv_eq(key, SV("texture"))) {
+                    texture_file_path = cstr_from_sv(value);
+                } else if (sv_eq(key, SV("mesh"))) {
+                    mesh_file_path = cstr_from_sv(value);
+                } else {
+                    printf("%s:%zu: WARNING: unknown key `"SV_Fmt"`\n",
+                           scene_conf_file_path, line_number,
+                           SV_Arg(key));
+                }
+            }
+        }
+
+        if (vertex_shader_file_path == NULL) {
+            fprintf(stderr, "ERROR: `vert_shader` is not specified in %s\n",
+                    scene_conf_file_path);
+            return;
+        }
+
+        if (fragment_shader_file_path == NULL) {
+            fprintf(stderr, "ERROR: `frag_shader` is not specified in %s\n",
+                    scene_conf_file_path);
+            return;
+        }
+
+        if (texture_file_path == NULL) {
+            fprintf(stderr, "ERROR: `texture` is not specified in %s\n",
+                    scene_conf_file_path);
+            return;
+        }
+
+        if (mesh_file_path == NULL) {
+            fprintf(stderr, "ERROR: `mesh` is not specified in %s\n",
+                    scene_conf_file_path);
+            return;
+        }
+
+    }
+    // reload scene.conf end
+
+    // reload shader program begin
+    {
+        glDeleteProgram(program);
+
+        GLuint vert = 0;
+        if (!compile_shader_file(vertex_shader_file_path, GL_VERTEX_SHADER, &vert)) {
+            return;
+        }
+
+        GLuint frag = 0;
+        if (!compile_shader_file(fragment_shader_file_path, GL_FRAGMENT_SHADER, &frag)) {
+            return;
+        }
+
+        if (!link_program(vert, frag, &program)) {
+            return;
+        }
+
+        glUseProgram(program);
+        time_location = glGetUniformLocation(program, "time");
+        resolution_location = glGetUniformLocation(program, "resolution");
+    }
+    // reload shader program end
+
+    // reload texture begin
+    {
+        glDeleteTextures(1, &texture_id);
+
+        int w, h;
+        uint32_t *pixels = (uint32_t*) stbi_load(texture_file_path, &w, &h, NULL, 4);
+        if (pixels == NULL) {
+            printf("ERROR: could not load file %s: %s\n", texture_file_path, strerror(errno));
+            return;
+        }
+
+        glGenTextures(1, &texture_id);
+        glBindTexture(GL_TEXTURE_2D, texture_id);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+        glTexImage2D(GL_TEXTURE_2D,
+                     0,
+                     GL_RGBA,
+                     w,
+                     h,
+                     0,
+                     GL_RGBA,
+                     GL_UNSIGNED_BYTE,
+                     pixels);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    // reload texture end
+
+    // reload model begin
+    {
+        glDeleteBuffers(VERTEX_ATTR_COUNT, buffer_ids);
+
+        String_View mesh_content = sv_from_cstr(slurp_file(mesh_file_path));
+        if (mesh_content.data == NULL) {
+            fprintf(stderr, "ERROR: could not read file `%s`: %s\n",
+                    mesh_file_path, strerror(errno));
+            return;
+        }
+
+        size_t positions_count = 0;
+        size_t uvs_count = 0;
+        for (size_t line_number = 0; mesh_content.count > 0; ++line_number) {
+            String_View line = sv_trim(sv_chop_by_delim(&mesh_content, '\n'));
+            line = sv_trim(sv_chop_by_delim(&line, '#'));
+
+            if (line.count > 0) {
+                String_View kind = sv_trim(sv_chop_by_delim(&line, ' '));
+
+                if (sv_eq(kind, SV("v"))) {
+                    memset(&vertex_positions[positions_count],
+                           0,
+                           sizeof(vertex_positions[positions_count]));
+                    for (size_t i = 0; i < V4_COMPS && line.count > 0; ++i) {
+                        String_View comp_sv = sv_trim(sv_chop_by_delim(&line, ' '));
+                        if (comp_sv.count > 0) {
+                            char *comp_cstr = cstr_from_sv(comp_sv);
+                            vertex_positions[positions_count].cs[i] = strtof(comp_cstr, NULL);
+                        }
+                    }
+                    positions_count += 1;
+                } else if (sv_eq(kind, SV("vt"))) {
+                    memset(&vertex_uvs[uvs_count],
+                           0,
+                           sizeof(vertex_uvs[uvs_count]));
+                    for (size_t i = 0; i < V2_COMPS && line.count > 0; ++i) {
+                        String_View comp_sv = sv_trim(sv_chop_by_delim(&line, ' '));
+                        if (comp_sv.count > 0) {
+                            char *comp_cstr = cstr_from_sv(comp_sv);
+                            vertex_uvs[uvs_count].cs[i] = strtof(comp_cstr, NULL);
+                        }
+                    }
+                    uvs_count += 1;
+                } else {
+                    fprintf(stderr, "%s:%zu: WARNING: unknown obj kind of line `"SV_Fmt"`\n",
+                            mesh_file_path, line_number, SV_Arg(kind));
+                }
+            }
+        }
+
+        vertex_size = positions_count;
+        if (vertex_size > uvs_count) {
+            vertex_size = uvs_count;
+        }
+
+        if (positions_count != uvs_count) {
+            fprintf(stderr, "%s: WARNING: the amount of positions (%zu) is not equal to the amount of uvs (%zu). We are gonna ignore this fact and just assume that there is only %zu vertices\n", mesh_file_path, positions_count, uvs_count, vertex_size);
+        }
+
+        glGenBuffers(VERTEX_ATTR_COUNT, buffer_ids);
+
+        {
+            glBindBuffer(GL_ARRAY_BUFFER, buffer_ids[VERTEX_ATTR_POSITION]);
+            glBufferData(GL_ARRAY_BUFFER,
+                         sizeof(vertex_positions[0]) * vertex_size,
+                         vertex_positions,
+                         GL_STATIC_DRAW);
+            glEnableVertexAttribArray(VERTEX_ATTR_POSITION);
+            glVertexAttribPointer(
+                VERTEX_ATTR_POSITION,   // index
+                V4_COMPS,           // numComponents
+                GL_FLOAT,           // type
+                0,                  // normalized
+                0,                  // stride
+                0                   // offset
+            );
+        }
+
+        {
+            glBindBuffer(GL_ARRAY_BUFFER, buffer_ids[VERTEX_ATTR_UV]);
+            glBufferData(GL_ARRAY_BUFFER,
+                         sizeof(vertex_uvs[0]) * vertex_size,
+                         vertex_uvs,
+                         GL_STATIC_DRAW);
+            glEnableVertexAttribArray(VERTEX_ATTR_UV);
+            glVertexAttribPointer(
+                VERTEX_ATTR_UV,     // index
+                V2_COMPS,           // numComponents
+                GL_FLOAT,           // type
+                0,                  // normalized
+                0,                  // stride
+                0                   // offset
+            );
+        }
+    }
+    // reload model end
+
     glClearColor(BACKGROUND_COLOR);
     program_failed = false;
-
-    String_View scene_conf_content = sv_from_cstr(slurp_file(scene_conf_file_path));
-    if (scene_conf_content.data == NULL) {
-        glClearColor(HOT_RELOAD_ERROR_COLOR);
-        program_failed = true;
-        return;
-    }
-
-    for (size_t line_number = 0; scene_conf_content.count > 0; line_number++) {
-        String_View line = sv_chop_by_delim(&scene_conf_content, '\n');
-        line = sv_trim(sv_chop_by_delim(&line, '#'));
-
-        if (line.count > 0) {
-            String_View key = sv_trim(sv_chop_by_delim(&line, '='));
-            String_View value = sv_trim(line);
-            if (sv_eq(key, SV("frag_shader"))) {
-                fragment_shader_file_path = cstr_from_sv(value);
-            } else if (sv_eq(key, SV("vert_shader"))) {
-                vertex_shader_file_path = cstr_from_sv(value);
-            } else if (sv_eq(key, SV("texture"))) {
-                texture_file_path = cstr_from_sv(value);
-            } else if (sv_eq(key, SV("mesh"))) {
-                mesh_file_path = cstr_from_sv(value);
-            } else {
-                printf("%s:%zu: WARNING: unknown key `"SV_Fmt"`\n",
-                       scene_conf_file_path, line_number,
-                       SV_Arg(key));
-            }
-        }
-    }
-
-    if (vertex_shader_file_path == NULL) {
-        fprintf(stderr, "WARNING: `vert_shader` is not specified in %s\n",
-                scene_conf_file_path);
-        glClearColor(HOT_RELOAD_ERROR_COLOR);
-        program_failed = true;
-        return;
-    }
-
-    if (fragment_shader_file_path == NULL) {
-        fprintf(stderr, "WARNING: `frag_shader` is not specified in %s\n",
-                scene_conf_file_path);
-        glClearColor(HOT_RELOAD_ERROR_COLOR);
-        program_failed = true;
-        return;
-    }
-
-    if (texture_file_path == NULL) {
-        fprintf(stderr, "WARNING: `texture` is not specified in %s\n",
-                scene_conf_file_path);
-        glClearColor(HOT_RELOAD_ERROR_COLOR);
-        program_failed = true;
-        return;
-    }
-
-    if (mesh_file_path == NULL) {
-        fprintf(stderr, "WARNING: `mesh` is not specified in %s\n",
-                scene_conf_file_path);
-        glClearColor(HOT_RELOAD_ERROR_COLOR);
-        program_failed = true;
-        return;
-    }
-
-    glDeleteProgram(program);
-
-    GLuint vert = 0;
-    if (!compile_shader_file(vertex_shader_file_path, GL_VERTEX_SHADER, &vert)) {
-        glClearColor(HOT_RELOAD_ERROR_COLOR);
-        program_failed = true;
-        return;
-    }
-
-    GLuint frag = 0;
-    if (!compile_shader_file(fragment_shader_file_path, GL_FRAGMENT_SHADER, &frag)) {
-        glClearColor(HOT_RELOAD_ERROR_COLOR);
-        program_failed = true;
-        return;
-    }
-
-    if (!link_program(vert, frag, &program)) {
-        glClearColor(HOT_RELOAD_ERROR_COLOR);
-        program_failed = true;
-        return;
-    }
-
-    glUseProgram(program);
-    time_location = glGetUniformLocation(program, "time");
-    resolution_location = glGetUniformLocation(program, "resolution");
-
-    glDeleteTextures(1, &texture_id);
-
-    int w, h;
-    uint32_t *pixels = (uint32_t*) stbi_load(texture_file_path, &w, &h, NULL, 4);
-    if (pixels == NULL) {
-        printf("ERROR: could not load file %s: %s\n", texture_file_path, strerror(errno));
-        glClearColor(HOT_RELOAD_ERROR_COLOR);
-        program_failed = true;
-        return;
-    }
-
-    glGenTextures(1, &texture_id);
-    glBindTexture(GL_TEXTURE_2D, texture_id);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-
-    glTexImage2D(GL_TEXTURE_2D,
-                 0,
-                 GL_RGBA,
-                 w,
-                 h,
-                 0,
-                 GL_RGBA,
-                 GL_UNSIGNED_BYTE,
-                 pixels);
-    glGenerateMipmap(GL_TEXTURE_2D);
-
-    // Reload mesh
-    glDeleteBuffers(VERTEX_ATTR_COUNT, buffer_ids);
-
-    String_View mesh_content = sv_from_cstr(slurp_file(mesh_file_path));
-    if (mesh_content.data == NULL) {
-        fprintf(stderr, "WARNING: could not read file `%s`: %s\n",
-                mesh_file_path, strerror(errno));
-        glClearColor(HOT_RELOAD_ERROR_COLOR);
-        program_failed = true;
-        return;
-    }
-
-    size_t positions_count = 0;
-    size_t uvs_count = 0;
-    for (size_t line_number = 0; mesh_content.count > 0; ++line_number) {
-        String_View line = sv_trim(sv_chop_by_delim(&mesh_content, '\n'));
-        line = sv_trim(sv_chop_by_delim(&line, '#'));
-
-        if (line.count > 0) {
-            String_View kind = sv_trim(sv_chop_by_delim(&line, ' '));
-
-            if (sv_eq(kind, SV("v"))) {
-                memset(&vertex_positions[positions_count],
-                       0,
-                       sizeof(vertex_positions[positions_count]));
-                for (size_t i = 0; i < V4_COMPS && line.count > 0; ++i) {
-                    String_View comp_sv = sv_trim(sv_chop_by_delim(&line, ' '));
-                    if (comp_sv.count > 0) {
-                        char *comp_cstr = cstr_from_sv(comp_sv);
-                        vertex_positions[positions_count].cs[i] = strtof(comp_cstr, NULL);
-                    }
-                }
-                positions_count += 1;
-            } else if (sv_eq(kind, SV("vt"))) {
-                memset(&vertex_uvs[uvs_count],
-                       0,
-                       sizeof(vertex_uvs[uvs_count]));
-                for (size_t i = 0; i < V2_COMPS && line.count > 0; ++i) {
-                    String_View comp_sv = sv_trim(sv_chop_by_delim(&line, ' '));
-                    if (comp_sv.count > 0) {
-                        char *comp_cstr = cstr_from_sv(comp_sv);
-                        vertex_uvs[uvs_count].cs[i] = strtof(comp_cstr, NULL);
-                    }
-                }
-                uvs_count += 1;
-            } else {
-                fprintf(stderr, "%s:%zu: WARNING: unknown obj kind of line `"SV_Fmt"`\n",
-                        mesh_file_path, line_number, SV_Arg(kind));
-            }
-        }
-    }
-
-    vertex_size = positions_count;
-    if (vertex_size > uvs_count) {
-        vertex_size = uvs_count;
-    }
-
-    if (positions_count != uvs_count) {
-        fprintf(stderr, "%s: WARNING: the amount of positions (%zu) is not equal to the amount of uvs (%zu). We are gonna ignore this fact and just assume that there is only %zu vertices\n", mesh_file_path, positions_count, uvs_count, vertex_size);
-    }
-
-    glGenBuffers(VERTEX_ATTR_COUNT, buffer_ids);
-
-    {
-        glBindBuffer(GL_ARRAY_BUFFER, buffer_ids[VERTEX_ATTR_POSITION]);
-        glBufferData(GL_ARRAY_BUFFER,
-                     sizeof(vertex_positions[0]) * vertex_size,
-                     vertex_positions,
-                     GL_STATIC_DRAW);
-        glEnableVertexAttribArray(VERTEX_ATTR_POSITION);
-        glVertexAttribPointer(
-            VERTEX_ATTR_POSITION,   // index
-            V4_COMPS,           // numComponents
-            GL_FLOAT,           // type
-            0,                  // normalized
-            0,                  // stride
-            0                   // offset
-        );
-    }
-
-    {
-        glBindBuffer(GL_ARRAY_BUFFER, buffer_ids[VERTEX_ATTR_UV]);
-        glBufferData(GL_ARRAY_BUFFER,
-                     sizeof(vertex_uvs[0]) * vertex_size,
-                     vertex_uvs,
-                     GL_STATIC_DRAW);
-        glEnableVertexAttribArray(VERTEX_ATTR_UV);
-        glVertexAttribPointer(
-            VERTEX_ATTR_UV,     // index
-            V2_COMPS,           // numComponents
-            GL_FLOAT,           // type
-            0,                  // normalized
-            0,                  // stride
-            0                   // offset
-        );
-    }
 
     printf("Successfully reloaded scene\n");
     printf("Memory %zu/%zu bytes\n", memory_size, (size_t) MEMORY_CAPACITY);
